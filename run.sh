8d82614d2dde444f85c18bb04d6b22ddc78dccec
wanghuacan <964280783@qq.com>
2025-08-13 14:02:00 +0000

    restruct

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 # python3 tree_code.py >repo3.md
 # python3 test_tree_sitter.py "./finance_agent" --setup-languages 
 
-# python3 core/agent_code_explore.py > logs/agent_code_explore.md
-# python3 core/code_explorer_tools.py > logs/code_explorer_tools.md
+# python3 src/core/agent_code_explore.py > logs/agent_code_explore.md
+# python3 src/core/code_explorer_tools.py > logs/code_explorer_tools.md
 
 
 # python3 test.py
 
-# python3 core/agent_code_explore.py
+# python3 src/core/agent_code_explore.py
 
 # python3 main.py 
 
@@ -19,8 +19,6 @@ export PYTHONPATH=$(pwd):$PYTHONPATH
 # python3 tasks/1_run_git.py --config config/sample_tasks.yaml
 # python3 tasks/1_run_git.py --config config/front_end_autogen.yaml
 
-# python3 core/tool_code_explorer.py
+# python3 src/core/agent_scheduler.py #> logs/agent_scheduler.md
 
-python3 core/agent_scheduler.py #> logs/agent_scheduler.md
-
-# streamlit run frontend_st/app_autogen_enhanced.py
+streamlit run frontend_st/app_autogen_enhanced.py
